e4b21e5a244718b6bb25ce3b46ad18febe7fb055
Dino Dev <honghai251.info@gmail.com>
2021-10-18 17:58:18 +0700

    Fix-Comment

--- components/SectionDescription/CommentCommon/index.js
+++ components/SectionDescription/CommentCommon/index.js
@@ -3,56 +3,69 @@ import { useSelector, useDispatch } from "react-redux";
 import PropTypes from "prop-types";
 import moment from "moment";
 import { Avatar, Form, Comment, Button, List, Input } from "antd";
+import router from "next/router";
 
 const propTypes = {};
 
+const { TextArea } = Input;
+const CommentList = ({ comments }) => (
+  <List
+    dataSource={comments}
+    header={`${comments.length} ${comments.length > 1 ? "replies" : "reply"}`}
+    itemLayout="horizontal"
+    renderItem={(props) => <Comment {...props} />}
+  />
+);
+
+const Editor = ({ onChange, onSubmit, submitting, value }) => (
+  <>
+    <Form.Item>
+      <TextArea rows={4} onChange={onChange} value={value} />
+    </Form.Item>
+    <Form.Item>
+      <Button
+        htmlType="submit"
+        loading={submitting}
+        onClick={onSubmit}
+        type="primary"
+      >
+        Bình Luận
+      </Button>
+    </Form.Item>
+  </>
+);
 const CommentCommon = (props) => {
+  //! State
   const [submmiting, setsubmmiting] = useState(false);
   const [comments, setcomments] = useState([]);
   const [values, setvalues] = useState("");
-  //! State
   const dispatch = useDispatch();
 
-  const { TextArea } = Input;
-
-  const CommentList = ({ comments }) => (
-    <List
-      dataSource={comments}
-      header={`${comments.length} ${comments.length > 1 ? "replies" : "reply"}`}
-      itemLayout="horizontal"
-      renderItem={(props) => <Comment {...props} />}
-    />
-  );
-
-  const Editor = ({ onChange, onSubmit, submitting, value }) => (
-    <>
-      <Form.Item>
-        <TextArea rows={4} onChange={onChange} value={value} />
-      </Form.Item>
-      <Form.Item>
-        <Button
-          htmlType="submit"
-          loading={submitting}
-          onClick={onSubmit}
-          type="primary"
-        >
-          Bình Luận
-        </Button>
-      </Form.Item>
-    </>
-  );
-  const ExampleComment = ({ children }) => (
+  const ExampleComment = ({
+    children,
+    content = "Chắc sẽ mua 1 chiếc",
+    author = "NamDo",
+  }) => (
     <Comment
-      actions={[<span key="comment-nested-reply-to">Reply to</span>]}
-      author={<a>NamDo</a>}
+      actions={[
+        <span
+          key="comment-nested-reply-to"
+          onClick={() => {
+            router.push("#editor");
+          }}
+        >
+          Trả lời
+        </span>,
+      ]}
+      author={<a>{author}</a>}
       avatar={<Avatar src="/static/assets/img/icon/avanho.png" alt="NamDo" />}
-      content={<p>Chắc sẽ mua 1 chiếc</p>}
+      content={<p>{content}</p>}
     >
       {children}
     </Comment>
   );
   //! Function
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
     if (!values) {
       return;
     }
@@ -69,7 +82,7 @@ const CommentCommon = (props) => {
           datetime: moment().fromNow(),
         },
       ]);
-    }, 100);
+    }, 1000);
   };
   const handleChange = (e) => {
     setvalues(e.target.value);
@@ -77,23 +90,29 @@ const CommentCommon = (props) => {
   //! Render
   return (
     <div className="comment-common">
-      <Comment
-        avatar={<Avatar src="/static/assets/img/icon/avanho.png" alt="NamDo" />}
-        content={
-          <Editor
-            onChange={handleChange}
-            onSubmit={() => {handleSubmit()}}
-            submitting={submmiting}
-            value={values}
-          />
-        }
-      />
+      <div id="editor">
+        <Comment
+          avatar={
+            <Avatar src="/static/assets/img/icon/avanho.png" alt="NamDo" />
+          }
+          content={
+            <Editor
+              onChange={handleChange}
+              onSubmit={() => {
+                handleSubmit();
+              }}
+              submitting={submmiting}
+              value={values}
+            />
+          }
+        />
+      </div>
       {comments.length > 0 && <CommentList comments={comments} />}
       {/*  */}
-      <ExampleComment>
-        <ExampleComment>
-          <ExampleComment />
-          <ExampleComment />
+      <ExampleComment content="Bao lâu nữa bán được 1 tỉ gói mè ?">
+        <ExampleComment author="Admin" content="Tôi bán kem đánh răng">
+          <ExampleComment content="Trả lời!" />
+          <ExampleComment author="Admin" content="Tôi bán kem đánh răng" />
         </ExampleComment>
       </ExampleComment>
       ,
